3992f3c46b8af52846990c5dd35e9ca1504c6fb7
Chris Joel <cdata@google.com>
2019-04-28 22:35:21 -0700

    Custom element mixin to help decorate shadow roots

--- rollup.config.js
+++ rollup.config.js
@@ -9,10 +9,28 @@ export default [
     }
   },
   {
+    input: 'src/custom-element-mixin.js',
+    output: {
+      file: 'dist/custom-element-mixin.js',
+      format: 'umd',
+      name: 'FocusVisiblePolyfillMixin'
+    }
+  },
+  {
     input: 'src/focus-visible.js',
     output: {
       file: 'dist/focus-visible.min.js',
       format: 'umd',
+      sourcemap: true
+    },
+    plugins: [uglify()]
+  },
+  {
+    input: 'src/custom-element-mixin.js',
+    output: {
+      file: 'dist/custom-element-mixin.min.js',
+      format: 'umd',
+      name: 'FocusVisiblePolyfillMixin',
       sourcemap: true
     },
     plugins: [uglify()]
---
+++ src/custom-element-mixin.js
@@ -0,0 +1,50 @@
+/**
+ * This is mixin function is designed to be applied to a class that inherits
+ * from HTMLElement. It makes it easy for a custom element to coordinate with
+ * the :focus-visible polyfill.
+ *
+ * @param {Function} SuperClass The base class implementation to decorate with
+ * implementation that coordinates with the :focus-visible polyfill
+ */
+export function FocusVisiblePolyfillMixin(SuperClass) {
+  var coordinateWithPolyfill = function(instance) {
+    // If there is no shadow root, there is no need to coordinate with the
+    // polyfill. If we already coordinated with the polyfill, we can skip
+    // subsequent invokcations:
+    if (
+      instance.shadowRoot == null ||
+      instance.hasAttribute('data-js-focus-visible') ||
+      (self.ShadyCSS != null && self.ShadyCSS.nativeShadow)
+    ) {
+      return;
+    }
+
+    // The polyfill might already be loaded. If so, we can apply it to the
+    // shadow root immediately:
+    if (self.applyFocusVisiblePolyfill) {
+      self.applyFocusVisiblePolyfill(instance.shadowRoot);
+    } else {
+      // Otherwise, wait for the polyfill to be loaded lazily. It might never
+      // be loaded, but if it is then we can apply it to the shadow root at
+      // the appropriate time by waiting for the ready event:
+      self.addEventListener(
+        'focus-visible-polyfill-ready',
+        function() {
+          self.applyFocusVisiblePolyfill(instance.shadowRoot);
+        },
+        { once: true }
+      );
+    }
+  };
+
+  // IE11 doesn't natively support custom elements or JavaScript class syntax
+  // The mixin implementation assumes that the user will take the appropriate
+  // steps to support both:
+  return class extends SuperClass {
+    // Attempt to coordinate with the polyfill when connected to the document:
+    connectedCallback() {
+      super.connectedCallback && super.connectedCallback();
+      coordinateWithPolyfill(this);
+    }
+  };
+}
